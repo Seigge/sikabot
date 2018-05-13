fbbf8f90125573b84f5816069c12b86de3852d87
Sike <default29@gmail.com>
2018-05-13 14:30:54 +0300

    mute update

--- commands/mbot.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client, message, args) => {
-    const ayy = client.emojis.find("name", "oldsika");
-    message.reply(`Neos руина ${ayy}`);
-}
--- commands/mute.js
+++ commands/mute.js
@@ -5,10 +5,12 @@ exports.run = (clients,message,args) =>
 
     if (!mutedMember)
         return console.log("The muted role does not exist");
+	
+	 if (!mutedMember && message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS") )
+        return console.log("The muted role dont exist and i can create it");
 
     if ( !message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS"))
         return message.reply("You dont have such permissions.");
-		return Promise.reject();
     if (message.mentions.members.size === 0)
         return message.reply("Укажите пользователя");
 
